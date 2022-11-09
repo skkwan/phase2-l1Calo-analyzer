24ca9ffc8f062ef968e31f161ad892d0d73ba061
Stephanie Kwan <skwan@princeton.edu>
2022-11-09 11:08:03 +0100

    wip: use TChain for single files, switch to EOS single input file
    Switch to TChain for single input file (one big hadd'd file in EOS), adjust plot axes

--- figures/parametricCurve/fillTH2D.h
+++ figures/parametricCurve/fillTH2D.h
@@ -2,6 +2,9 @@
 #include "TTreeReader.h"
 #include "TTreeReaderValue.h"
 
+#include "../baseCodeForPlots/getTChain.h"
+
+
 #ifndef FILL_TH2D_H_INCL
 #define FILL_TH2D_H_INCL
 
@@ -11,28 +14,17 @@
 
 TH2D* fillTH2DIsolationVsPt(TString rootFileDirectory, TString treePath) {
 
-    // Load file
-    TFile *file = new TFile(rootFileDirectory);
-    if (!file->IsOpen() || file==0 ) {
-      std::cout<<"ERROR: FILE "<< rootFileDirectory <<" NOT FOUND; EXITING"<<std::endl;
-      return NULL;
-    }
-
-    TTree* tree = (TTree*)file->Get(treePath);
-    if (tree == 0) {
-      std::cout<<"ERROR: Tree " << tree << " NOT FOUND; EXITING"<<std::endl;
-      return NULL;
-    }
+    TChain *ch = getTChainFromSingleFile(rootFileDirectory, treePath);
 
     // Declare the branches we want to read
-    TTreeReader myReader(treePath, file);
+    TTreeReader myReader(ch);
     TTreeReaderValue<Double_t> reader_iso(myReader, "gct_iso");
     TTreeReaderValue<Double_t> reader_cPt(myReader, "gct_cPt");
 
     // Create and fill a 2D histogram
-    TH2D *h2 = new TH2D("h2", "h2", 20, 0.0, 100.0, 100, 0.0, 50.0);
+    TH2D *h2 = new TH2D("h2", "h2", 20, 0.0, 100.0, 100, 0.0, 11.0);
 
-    // Loop over all entries of the TTree
+    // Loop over all entries of the TChain
     while (myReader.Next()) {
       // Access data as if iso and cPt were iterators (note the '*' in front of them)
       // std::cout << "iso and cpt:" << *reader_iso << " and " << *reader_cPt << std::endl;
@@ -50,34 +42,23 @@ TH2D* fillTH2DIsolationVsPt(TString rootFileDirectory, TString treePath) {
 
 TH2D* fillTH2DShapeVarVsPt(TString rootFileDirectory, TString treePath) {
 
-    // Load file
-    TFile *file = new TFile(rootFileDirectory);
-    if (!file->IsOpen() || file==0 ) {
-      std::cout<<"ERROR: FILE "<< rootFileDirectory <<" NOT FOUND; EXITING"<<std::endl;
-      return NULL;
-    }
+  TChain *ch = getTChainFromSingleFile(rootFileDirectory, treePath);
 
-    TTree* tree = (TTree*)file->Get(treePath);
-    if (tree == 0) {
-      std::cout<<"ERROR: Tree " << tree << " NOT FOUND; EXITING"<<std::endl;
-      return NULL;
-    }
+  // Declare the branches we want to read
+  TTreeReader myReader(ch);
+  TTreeReaderValue<Double_t> reader_cPt(myReader, "gct_cPt");
+  TTreeReaderValue<Double_t> reader_et2x5(myReader, "gct_et2x5");
+  TTreeReaderValue<Double_t> reader_et5x5(myReader, "gct_et5x5");
 
-    // Declare the branches we want to read
-    TTreeReader myReader(treePath, file);
-    TTreeReaderValue<Double_t> reader_cPt(myReader, "gct_cPt");
-    TTreeReaderValue<Double_t> reader_et2x5(myReader, "gct_et2x5");
-    TTreeReaderValue<Double_t> reader_et5x5(myReader, "gct_et5x5");
+  // Create and fill a 2D histogram
+  TH2D *h2 = new TH2D("Shape var", "Shape var", 20, 0.0, 100.0, 50, 0.4, 1.05);
 
-    // Create and fill a 2D histogram
-    TH2D *h2 = new TH2D("Shape var", "Shape var", 20, 0.0, 100.0, 50, 0.0, 1.1);
+  // Loop over all entries of the TChain
+  while (myReader.Next()) {
+    h2->Fill(*reader_cPt, *reader_et2x5/ *reader_et5x5);
+  }
 
-    // Loop over all entries of the TTree
-    while (myReader.Next()) {
-      h2->Fill(*reader_cPt, *reader_et2x5/ *reader_et5x5);
-    }
-
-    return h2;
+  return h2;
 
 }
 
