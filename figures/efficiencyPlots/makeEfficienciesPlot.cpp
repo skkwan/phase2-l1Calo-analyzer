9b82f2a5360fa93b74669e2ce44abf096eab7bad
Stephanie Kwan <skwan@princeton.edu>
2022-11-10 16:21:07 +0100

    study: show efficiencies for L1 pT > 5 GeV
    Additional study only for debugging isolation performance

--- figures/efficiencyPlots/makeEfficienciesPlot.cpp
+++ figures/efficiencyPlots/makeEfficienciesPlot.cpp
@@ -97,7 +97,43 @@ void makeEfficienciesPlot(void)
                     "Gen Electron #eta",
                     "Phase 2 GCT",                                                                
                     "efficiency_genEta_barrel_GCT",        
-                    outputDirectory, "", 0.0, 1.1);    
+                    outputDirectory, "L1 p_{T} > 25, |#eta^{Gen}| < 1.4841", 0.0, 1.1);    
+
+  /*******************************************************/
+  /* Additional studies                                  */
+  /*******************************************************/
+
+  // This is weird -- needs to be run with the above plots commented out
+
+  vGraphs.clear();  vLabels.clear();  vColors.clear();
+  xMin = 0;
+  xMax = 100;
+  genCut  = "(abs(genEta) < 1.4841)";
+  l1Cut   = "(abs(genEta) < 1.4841) && (gct_cPt > 5)";
+  useVariableBinning = false;
+
+  TGraphAsymmErrors *allAdditional = calculateEfficiency("genPt", treePath, rootFileDirectory,
+              l1Cut,
+              genCut, xMin, xMax, useVariableBinning);
+  vGraphs.push_back(allAdditional);
+  vLabels.push_back("w/o iso and shower shape");
+  vColors.push_back(kBlack);
+
+  TGraphAsymmErrors *tightAdditional = calculateEfficiency("genPt", treePath, rootFileDirectory,
+                                                  l1Cut + "&& gct_is_iso && gct_is_ss",
+                                                  genCut, xMin, xMax, useVariableBinning);
+  vGraphs.push_back(tightAdditional);
+  vLabels.push_back("with iso and shower shape");
+  vColors.push_back(kRed);
+
+
+  plotNEfficiencies(vGraphs, vLabels, vColors,
+                    "Gen Electron p_{T}",
+                    "Phase 2 GCT",                                                                
+                    "efficiency_genPt_barrel_GCT_l1Pt_gt_5GeV",        
+                    outputDirectory, "L1 p_{T} > 5, |#eta^{Gen}| < 1.4841", 0.8, 1.02);    
+  
+
    
 }
 /*********************************************************************/
