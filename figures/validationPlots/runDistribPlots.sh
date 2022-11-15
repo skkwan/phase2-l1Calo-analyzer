cb0ec2f98f95c118f8441def5fb821201cefd4d3
Stephanie Kwan <skwan@princeton.edu>
2022-11-15 18:10:56 +0100

    chore: switch to latest n-tuple

--- figures/validationPlots/runDistribPlots.sh
+++ figures/validationPlots/runDistribPlots.sh
@@ -22,7 +22,7 @@
 
 root -l -b -q 'makeDistributionPlots.C("SF_1.00_cPt-gt-15_genPt-gt-15",                                             \
                                        "DoubleElectron Flat Pt 2 to 100",
-                                       "/eos/user/s/skkwan/phase2RCTDevel/analyzer_DoubleElectron.root", \
+                                       "/eos/user/s/skkwan/phase2RCTDevel/analyzer_DoubleElectron_partial.root", \
                                        "/eos/user/s/skkwan/phase2RCTDevel/figures/validation/", \
                                        "(gct_cPt > 15) && (genPt > 15) && (abs(genEta) < 1.4841)", "SF = 1.0",     \
                                        "L1 p_{T} > 15, Gen p_{T} > 15",                                              \
