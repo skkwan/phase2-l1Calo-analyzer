3a96896e9a8038e6ee31987c04e280f25b11788d
Stephanie Kwan <skwan@princeton.edu>
2022-11-15 18:00:42 +0100

    study: switch to 99% acceptance in cut-off derivation

--- figures/parametricCurve/cutoffValues.h
+++ figures/parametricCurve/cutoffValues.h
@@ -20,8 +20,8 @@ double getCutoffValue(TH2D* h2, int iBinX, bool countUpwards = true) {
         nEvents += h2->GetBinContent(iBinX, iBinY);
     }
 
-    // 98% cutoff
-    double nEventsBelowCutoff = nEvents * 0.98;
+    // 99% cutoff
+    double nEventsBelowCutoff = nEvents * 0.99;
 
     std::cout << "nEvents and nEvents below cut-off: " << nEvents << ", " << nEventsBelowCutoff << std::endl;
 
