9f556856a52ae669a79fdf205bc669c68838c41f
Stephanie Kwan <skwan@princeton.edu>
2023-01-23 15:33:32 +0100

    feature: add compare scripts
    Add scripts to run comparisons between n-tuples.

---
+++ compare/varCfg.py
@@ -0,0 +1,11 @@
+var_dict = {
+    'gct_cPt':    {'nbinsx':50, 'xmin':0., 'xmax':120., 'title':'Reco cluster p_{T} (GeV)'},
+    'gct_deltaR': {'nbinsx':25, 'xmin':0., 'xmax':0.1},
+    'gct_is_ss':  {'nbinsx':2, 'xmin':0., 'xmax':1., 'title':'Is ss (TDR flag definition)'},
+    'gct_is_iso': {'nbinsx':2, 'xmin':0., 'xmax':1., 'title':'Is iso (TDR flag definition)'},
+    'gct_is_looseTkss':  {'nbinsx':2, 'xmin':0., 'xmax':1., 'title':'Is Loose Tk ss (TDR flag definition)'},
+    'gct_is_looseTkiso':  {'nbinsx':2, 'xmin':0., 'xmax':1., 'title':'Is Loose Tk iso (TDR flag definition)'},
+    'gct_iso':    {'nbinsx':50, 'xmin':0., 'xmax':1., 'title':'Relative isolation'},
+
+}
+
