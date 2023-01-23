549fc57a6a398453cbce877c523bf23b30df15b9
Stephanie Kwan <skwan@princeton.edu>
2023-01-23 15:35:55 +0100

    feature: add script to pick list of events
    Add script to make command which picks a list of events (generated by
    compare.py).

---
+++ pickEvent/runPickEventCRAB.sh
@@ -0,0 +1,4 @@
+# Reference: https://twiki.cern.ch/twiki/bin/view/CMSPublic/WorkBookPickEvents#Run_edm_PickEvents_py_with_CRAB
+# Basic usage: run this script, and then copy-paste the resulting command into the command line.
+
+edmPickEvents.py "/MinBias_TuneCP5_14TeV-pythia8/Phase2HLTTDRWinter20DIGI-PU200_110X_mcRun4_realistic_v3-v3/GEN-SIM-DIGI-RAW" eventsWithDiff.txt
